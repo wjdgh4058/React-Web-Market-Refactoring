58303fc55da8b125bba2c59a04b63da6e6169c7c
leejungho <ljhmoa@gmail.com>
2023-05-20 23:04:03 +0900

    [Update] 불필요한 주석 삭제

--- src/components/CartPageAccordion/CartPageAccordion.jsx
+++ src/components/CartPageAccordion/CartPageAccordion.jsx
@@ -25,7 +25,6 @@ export function CartPageAccordion({ data }) {
     roomTemperature = storingWaySort(data, '상온');
   }
 
-  //   useRecoilState(cartTotalSeletState);
   // useEffect(() => {
   //   setCheckTotalCount(data.length);
   // }, data);
